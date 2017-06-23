2254a061d53b5aeeebb5a61a34a0983be0f509bf
vero <vero.boulnois@gmail.com>
2017-06-23 14:51:48 +0200

    debut projet

---
+++ index.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width= device-width,initial-scale=1">
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
+        <link href="assets/css/style.css" rel="stylesheet">
+        <script src="https://use.fontawesome.com/a85a823305.js"></script>
+        <script>
+            (function (i, s, o, g, r, a, m) {
+                i['GoogleAnalyticsObject'] = r;
+                i[r] = i[r] || function () {
+                    (i[r].q = i[r].q || []).push(arguments)
+                }, i[r].l = 1 * new Date();
+                a = s.createElement(o),
+                        m = s.getElementsByTagName(o)[0];
+                a.async = 1;
+                a.src = g;
+                m.parentNode.insertBefore(a, m)
+            })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
+            ga('create', 'UA-91921684-1', 'auto');
+            ga('send', 'pageview');
+        </script>
+        <title>Accueil</title>
+    </head>
+    <body>
+        <div class="container">
+            <header>
+                <div class="col-lg-8 colcol-md-8 col-sm-12 col-xs-12"><h1>Michel BOULNOIS</h1><h2>SCULPTEUR SUR PIERRE ET BOIS</h2></div>
+                <div class="col-lg-4 col-md-4 col-sm-12 col-xs-12"><img src=".jpg" alt="photosculpture" title="sculpture"></div>
+            </header>
+            <div class="row">
+                <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                    <nav class="navbar navbar-inverse">
+                        <ul class="nav navbar-nav">
+                            <li class="active"><a href="#"><h4><strong>Accueil</strong></h4></a></li>
+                            <li class="menu"><a href="index2.html"><h4><strong>A propos</strong></h4></a></li>
+                            <li class="menu"><a href="index3.html"><h4><strong>Oeuvres</strong></h4></a></li>
+                            <li class="menu"><a href="index4.html"><h4><strong> Tuto</strong></h4></a></li>
+                            <li class="menu"><a href="index5.html"><h4><strong>Expositions</strong></h4></a></li>
+                            <li class="menu"><a href="index6.html"><h4><strong>Contact</strong></h4></a></li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+    </body>
+</html>
